4238ff1c5bc1345b81bab8330f9fe771f40cc500
Patrick Dolan <dolanp1992@gmail.com>
2022-05-28 17:54:48 -0700

    refactor form and ticket system to use state

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { v4 } from "uuid";
+import PropTypes from "prop-types";
 
 function NewTicketForm(props){
 
   function handleNewTicketFormSubmission(event) {
     event.preventDefault();
-    console.log(event.target.names.value);
-    console.log(event.target.location.value);
-    console.log(event.target.issue.value);
+    props.onNewTicketCreation({
+      names: event.target.names.value,
+      location: event.target.location.value,
+      issue: event.target.issue.value,
+      id: v4()
+    })
   }
 
   return (
@@ -28,6 +32,10 @@ function NewTicketForm(props){
       </form>
     </React.Fragment>
   );
+}
+
+NewTicketForm.propTypes = {
+  onNewTicketCreation: PropTypes.func
 }
 
 export default NewTicketForm;
